ce89de14cd34a775fc8843295a3729757ae47738
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-08-01 17:29:46 -0700

    reading from hardcoded input file and set up compression function using string package

--- OneLiner.go
+++ OneLiner.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	input := []byte("test test test test \n test again")
-	err := os.WriteFile("result.txt", input, 0777)
+	content, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		log.Fatalf("Error while reading %v", err)
+	}
+
+	//will call function "Compress Extension" that will take 'content' variable read from the input file and compress that onto a single line
+	//that can then be given to the Write file function for output to result.txt
+	input := CompressExtension(content)
+
+	err = os.WriteFile("result.txt", input, 0777)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
+}
+
+func CompressExtension(input []byte) []byte {
+	input = []byte(strings.Replace(string(input), "\n", " ", -1))
+	input = []byte(strings.Replace(string(input), "\r", "", -1))
+	return input
 }
